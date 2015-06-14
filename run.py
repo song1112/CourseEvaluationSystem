b138c096db9ccb6fc2e16c969d2b28ae5dd6c81f
Song <sooon1g1@gmail.com>
2015-06-15 03:25:03 +0800

    test commit

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import *
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
+            error= "sorry"
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html',error=error)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -13,6 +24,10 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
+@app.route('/fluid')
+def fluid():
+    return render_template('fluid.html')
+
 #test
 @app.route('/user/<name>')
 def user(name):
@@ -20,3 +35,4 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
